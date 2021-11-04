673f872c6bb3535bdcd515670e61234f56054fe6
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-04 14:38:32 +0800

    total amount

--- program3ver3.py
+++ program3ver3.py
@@ -6,6 +6,11 @@ def getorangeQty():
     _orangeQty = int(input("How many orange? "))
     return _orangeQty
 
+def gettotalAmount():
+    apple_amount = int(20)*appleQty
+    orange_amount = int(25)*orangeQty
+    return apple_amount + orange_amount
 
 appleQty = getappleQty()
 orangeQty = getorangeQty()
+totalAmount = gettotalAmount()
