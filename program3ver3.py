1b5d3daf1242cdbd697f96cd3fd13e00aa7b3ffa
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-04 14:33:40 +0800

    quantity

---
+++ program3ver3.py
@@ -0,0 +1,11 @@
+def getappleQty():
+    _appleQty = int(input("How many apple? "))
+    return _appleQty
+
+def getorangeQty():
+    _orangeQty = int(input("How many orange? "))
+    return _orangeQty
+
+
+appleQty = getappleQty()
+orangeQty = getorangeQty()
