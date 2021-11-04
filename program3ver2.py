5e2ad783cd80605b6d777d518d0f4b1d12c00a5c
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-04 14:48:33 +0800

    computation

--- program3ver2.py
+++ program3ver2.py
@@ -6,11 +6,23 @@ def getpriceApple():
     priceApple = int(input("Enter the price of apple: "))
     return priceApple
 
+def getquantityApple():
+    quantityApple =int(raw_amount/price_apple)
+    return quantityApple
+
+def gettotalPayment():
+    totalPayment = int(price_apple*quantity_apple)
+    return totalPayment
+
+def gettotalAmount():
+    totalAmount = int(raw_amount%total_payment)
+    return totalAmount
 
 
-def display(quantity_apple, total_amount):
-    print(f"You can buy {quantity_apple} apples and your change is {total_amount} pesos.")
 
 
 raw_amount = getrawAmount()
 price_apple = getpriceApple()
+quantity_apple = getquantityApple()
+total_payment = gettotalPayment()
+total_amount = gettotalAmount()
